89f23b57c3802e09ae57de6b3d4138c0612e0c2f
lucas <souza>
2019-07-03 20:09:49 -0400

    Sprint 10 - Supplier order received email doesn't have link to platform in it.

--- resources/views/emails/orders/new_order_suppliers.blade.php
+++ resources/views/emails/orders/new_order_suppliers.blade.php
@@ -9,6 +9,7 @@
 
 <p>
     A new order has been received for which your company qualifies. To review the order and place a bid, please click the link below.
+    <a href="{{ route('supplier.orders.index') }}">LINK TO PLATFORM</a>
 </p>
 
 @extends('emails.footer')
